9c98b5cfc16c9df4b5ccdd662f4b15a16406f866
Jonathan Moraes <arkye@hotmail.com.br>
2015-04-08 22:41:59 -0300

    Applying Test Activities

--- app/src/main/java/com/team_one/modesteam/bazinga/MainActivity.java
+++ app/src/main/java/com/team_one/modesteam/bazinga/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team_one.modesteam.bazinga;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -18,11 +19,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -48,19 +44,9 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+	public void showHomeActivity(View view) {
+		Intent intent = new Intent();
+		intent.setClass(this, HomeActivity.class);
+		startActivity(intent);
+	}
 }
