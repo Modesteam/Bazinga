99cac9db7a119b24df3b9ead907169cd48499a8c
Arkye <arkye@hotmail.com.br>
2015-10-18 19:24:38 -0400

    Relative Scale in Button Text

--- core/src/com/modesteam/bazinga/Bazinga.java
+++ core/src/com/modesteam/bazinga/Bazinga.java
@@ -11,72 +11,91 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class Bazinga extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Skin skin;
-	private Stage stage;
+    private SpriteBatch batch;
+    private Skin skin;
+    private Stage stage;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
-		stage = new Stage();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+        stage = new Stage();
 
-		final TextButton button = new TextButton("Start a Game!", skin, "default");
-		button.setWidth(Gdx.graphics.getWidth());
-		button.setHeight(Gdx.graphics.getHeight()/15);
-		button.setPosition(0, Gdx.graphics.getHeight()/4);
-		button.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				button.setText("Pressed: Start a Game!");
-			}
-		});
+        final TextButton startButton = new TextButton("Start a Game!", skin, "default");
+        startButton.getLabel().setFontScale(Gdx.graphics.getWidth() / 240,
+                Gdx.graphics.getHeight() / 320);
+        startButton.setWidth(Gdx.graphics.getWidth());
+        startButton.setHeight(Gdx.graphics.getHeight() / 15);
+        startButton.setPosition(0, Gdx.graphics.getHeight() / 4);
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                startButton.setText("Pressed: Start a Game!");
+            }
+        });
 
-		final TextButton instructionButton = new TextButton("How To Play?", skin, "default");
-		instructionButton.setWidth(Gdx.graphics.getWidth());
-		instructionButton.setHeight(Gdx.graphics.getHeight()/15);
-		instructionButton.setPosition(0,
-				button.getY() - instructionButton.getHeight());
-		instructionButton.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				instructionButton.setText("Pressed: How To Play?");
-			}
-		});
+        final TextButton instructionButton = new TextButton("How To Play?", skin, "default");
+        instructionButton.getLabel().setFontScale(Gdx.graphics.getWidth() / 240,
+                Gdx.graphics.getHeight() / 320);
+        instructionButton.setWidth(Gdx.graphics.getWidth());
+        instructionButton.setHeight(Gdx.graphics.getHeight() / 15);
+        instructionButton.setPosition(0,
+                startButton.getY() - instructionButton.getHeight());
+        instructionButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                instructionButton.setText("Pressed: How To Play?");
+            }
+        });
 
-		stage.addActor(button);
-		stage.addActor(instructionButton);
+        final TextButton exitButton = new TextButton("Exit The Game", skin, "default");
+        exitButton.getLabel().setFontScale(Gdx.graphics.getWidth() / 240,
+                Gdx.graphics.getHeight() / 320);
+        exitButton.setWidth(Gdx.graphics.getWidth());
+        exitButton.setHeight(Gdx.graphics.getHeight() / 15);
+        exitButton.setPosition(0,
+                instructionButton.getY() - exitButton.getHeight());
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                exitButton.setText("Pressed: Exit The Game");
+            }
+        });
 
-		Gdx.input.setInputProcessor(stage);
-	}
+        stage.addActor(startButton);
+        stage.addActor(instructionButton);
+        stage.addActor(exitButton);
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-	}
+        Gdx.input.setInputProcessor(stage);
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 
-		batch.begin();
-		stage.draw();
-		batch.end();
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	@Override
-	public void resize(int width, int height) {
-		// Nothing
-	}
+        batch.begin();
+        stage.draw();
+        batch.end();
+    }
 
-	@Override
-	public void pause() {
-		// Nothing
-	}
+    @Override
+    public void resize(int width, int height) {
+        // Nothing
+    }
 
-	@Override
-	public void resume() {
-		// Nothing
-	}
+    @Override
+    public void pause() {
+        // Nothing
+    }
+
+    @Override
+    public void resume() {
+        // Nothing
+    }
 }
